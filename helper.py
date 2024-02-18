7b6c02a9798181832d3cfd3e99f7367a8f4bcda8
alan890104 <alan890104@gmail.com>
2024-02-18 17:28:12 +0800

    feat: add spinner and parallel initialize client

--- helper.py
+++ helper.py
@@ -5,24 +5,34 @@ from pytoncenter.extension.message import JettonMessage
 from pytoncenter.address import Address
 from tonpy import CellSlice
 import asyncio
+import itertools
+
+DEFAULT_MAP = {
+    Address("kQBXgF5mlp3AY7eg1jc6gsWbpCdkzs8EvfkLR3mzIZ2xT1Ys"): "Oracle (TON/USDT)",
+    Address("kQBdt8NeOWfqcFwKhckX--6JuuTr4O8MhEicaKHeJ5Qyfpzt"): "Oracle Jetton Wallet (USDT)",
+}
+
+
+async def spinner(msg: str = "Loading..."):
+    """Asynchronous spinner function."""
+    for frame in itertools.cycle(["-", "\\", "|", "/"]):
+        print(f"{msg} {frame}", end="\r", flush=True)
+        await asyncio.sleep(0.2)
 
 
 async def wait_tick_success(client: AsyncTonCenterClientV3, msg_hash: str, user_address: str):
-    map = {
-        Address(user_address): "Your Wallet",
-        Address("kQBXgF5mlp3AY7eg1jc6gsWbpCdkzs8EvfkLR3mzIZ2xT1Ys"): "Oracle (TON/USDT)",
-        Address("kQBdt8NeOWfqcFwKhckX--6JuuTr4O8MhEicaKHeJ5Qyfpzt"): "Oracle Jetton Wallet (USDT)",
-    }
-    while True:
-        txs = await client.get_transaction_by_message(GetTransactionByMessageRequest(direction="in", msg_hash=msg_hash))
-        for i in range(5):
-            print("Waiting for tick success" + "." * i, end="\r")
-            await asyncio.sleep(1)
-        if len(txs) == 0:
-            continue
-        assert len(txs) == 1
-        print("Analyzing transaction...")
-        trace = await client.get_trace_alternative(GetTransactionTraceRequest(hash=txs[0].hash))
+    map = {**DEFAULT_MAP, Address(user_address): "Your Wallet"}
+    spinner1 = asyncio.create_task(spinner("Wait for the transaction to be confirmed"))
+
+    try:
+        tx = await anext(client.wait_message_exists(WaitMessageExistsRequest(msg_hash=msg_hash)))
+    finally:
+        spinner1.cancel()
+        print()
+
+    spinner2 = asyncio.create_task(spinner("Analyzing transaction"))
+    try:
+        trace = await client.get_trace_alternative(GetTransactionTraceRequest(hash=tx.hash))
         user_jetton_wallet = Address(trace.children[0].transaction.in_msg.destination)
         map.update({user_jetton_wallet: "Your Jetton Wallet"})
         address_mapping = create_address_mapping(map)
@@ -46,3 +56,32 @@ async def wait_tick_success(client: AsyncTonCenterClientV3, msg_hash: str, user_
                 print()
                 print(format_trace(trace, address_mapping=address_mapping))
                 return
+    finally:
+        spinner2.cancel()
+
+
+async def wait_ring_success(client: AsyncTonCenterClientV3, msg_hash: str):
+    spinner_task = asyncio.create_task(spinner("Wait for the transaction to be confirmed"))
+    try:
+        tx = await anext(client.wait_message_exists(WaitMessageExistsRequest(msg_hash=msg_hash)))
+        print()
+        print("Transaction Found \033[93m(0xc3510a29)\033[0m:")
+        print(f"\033[93mhttps://testnet.tonviewer.com/transaction/{tx.hash}\033[0m")
+        print()
+        return
+    finally:
+        spinner_task.cancel()
+
+
+async def wait_wind_success(client: AsyncTonCenterClientV3, msg_hash: str):
+    spinner_task = asyncio.create_task(spinner("Wait for the transaction to be confirmed"))
+    try:
+        tx = await anext(client.wait_message_exists(WaitMessageExistsRequest(msg_hash=msg_hash)))
+        print()
+        print("Analyzing transaction...")
+        print("Transaction Found \033[93m(0x9e5d3112)\033[0m:")
+        print(f"\033[93mhttps://testnet.tonviewer.com/transaction/{tx.hash}\033[0m")
+        print()
+        return
+    finally:
+        spinner_task.cancel()
--- main.py
+++ main.py
@@ -7,12 +7,24 @@ import logging
 from dotenv import load_dotenv
 import sys
 import os
-from helper import wait_tick_success
+from helper import wait_tick_success, wait_ring_success, wait_wind_success
+from functools import cache
 
 load_dotenv()
 mnemonics = os.getenv("WALLET_MNEMONICS")
 toncenter_api_key = os.getenv("TONCENTER_API_KEY")
 LOGGER = logging.getLogger(__name__)
+
+
+@cache
+async def init_client() -> TicTonAsyncClient:
+    return await TicTonAsyncClient.init(
+        mnemonics=mnemonics,
+        toncenter_api_key=toncenter_api_key,
+        oracle_addr="kQBXgF5mlp3AY7eg1jc6gsWbpCdkzs8EvfkLR3mzIZ2xT1Ys",
+        testnet=True,
+        logger=LOGGER,
+    )
 
 
 async def tick(client: TicTonAsyncClient, price: float):
@@ -26,88 +38,97 @@ async def tick(client: TicTonAsyncClient, price: float):
 
 
 async def ring(client: TicTonAsyncClient, alarm_id: int):
-    txhash = await client.ring(alarm_id)
-    print(txhash)
+    sent_msg = await client.ring(alarm_id)
+    print(sent_msg)
+    await wait_ring_success(client.toncenter, msg_hash=sent_msg.message_hash)
 
 
 async def wind(client: TicTonAsyncClient, alarm_id: int, buy_num: int, price: float):
-    txhash = await client.wind(alarm_id, buy_num, price)
-    print(txhash)
+    sent_msg = await client.wind(alarm_id, buy_num, price)
+    print(sent_msg)
+    await wait_wind_success(client.toncenter, msg_hash=sent_msg.message_hash)
 
 
 async def main():
-    print("Please choose the function you want to execute: ")
-    print("0. generate new wallet")
-    print("1. tick")
-    print("2. ring")
-    print("3. wind")
-    choice = int(input("Enter your choice (0/1/2/3): "))
+    client_task = asyncio.create_task(init_client())
+    show_prompt = False
 
-    if choice == 0:
-        wallet_mnemonic = mnemonic_new()
-        version = WalletVersionEnum.v4r2
-        _, _, _, wallet = Wallets.from_mnemonics(wallet_mnemonic, version)
-        print("\033[95m===============Wallet Information===============\033[0m")
-        print(
-            "Wallet Mnemonics: \n", "\033[93m" + " ".join(wallet_mnemonic) + "\033[0m"
-        )
-        print(
-            "Wallet Address: \n",
-            "\033[93m" + wallet.address.to_string(True, True, True) + "\033[0m",
-        )
-        print("\033[95m===============Instructions===============\033[92m")
-        print("⭐️ Please keep the wallet mnemonics in a safe place ⭐️")
-        print(
-            "1. Import the mnemonics to your wallet software. (e.g. Browser Extension, TonKeeper, TonSpace, etc.)"
-        )
-        print(
-            "2. Copy your address and go to https://t.me/testgiver_ton_bot to get some testnet TON."
-        )
-        print("3. Use the wallet address to receive TON from TestGiver.")
-        print("4. Remember to edit `.env` with your new mnemonic\033[0m")
-        print("🥳🥳🥳 Done 🥳🥳🥳")
-        return
+    while True:
+        if show_prompt:
+            while True:
+                choice = input("Would you like to continue? ([Y, any key]/n): ")
+                if choice.lower() == "n":
+                    return
+                break
+        show_prompt = True
 
-    client = await TicTonAsyncClient.init(
-        mnemonics=mnemonics,
-        toncenter_api_key=toncenter_api_key,
-        oracle_addr="kQBXgF5mlp3AY7eg1jc6gsWbpCdkzs8EvfkLR3mzIZ2xT1Ys",
-        testnet=True,
-        logger=LOGGER,
-    )
+        print("Please choose the function you want to execute: ")
+        print("0. generate new wallet")
+        print("1. tick")
+        print("2. ring")
+        print("3. wind")
+        raw_choice = input("Enter your choice (0/1/2/3): ")
+        if raw_choice not in {"0", "1", "2", "3"}:
+            print("Invalid choice, please try again.")
+            continue
+        
+        choice = int(raw_choice)
+        if choice == 0:
+            wallet_mnemonic = mnemonic_new()
+            version = WalletVersionEnum.v4r2
+            _, _, _, wallet = Wallets.from_mnemonics(wallet_mnemonic, version)
+            print("\033[95m===============Wallet Information===============\033[0m")
+            print("Wallet Mnemonics: \n", "\033[93m" + " ".join(wallet_mnemonic) + "\033[0m")
+            print(
+                "Wallet Address: \n",
+                "\033[93m" + wallet.address.to_string(True, True, True) + "\033[0m",
+            )
+            print("\033[95m===============Instructions===============\033[92m")
+            print("⭐️ Please keep the wallet mnemonics in a safe place ⭐️")
+            print("1. Import the mnemonics to your wallet software. (e.g. Browser Extension, TonKeeper, TonSpace, etc.)")
+            print("2. Copy your address and go to https://t.me/testgiver_ton_bot to get some testnet TON.")
+            print("3. Use the wallet address to receive TON from TestGiver.")
+            print("4. Remember to edit `.env` with your new mnemonic\033[0m")
+            print("🥳🥳🥳 Done 🥳🥳🥳")
+            continue
 
-    if choice == 1:
-        price = float(input("Enter the price: "))
-        await tick(client, price)
-    elif choice == 2:
-        alarm_id = int(input("Enter the alarm id: "))
-        await ring(client, alarm_id)
-    elif choice == 3:
-        options = f"latest: {client.metadata.total_alarms-1}"
-        alarm_id = int(input(f"Enter the alarm id ({options}): "))
-        print("Loading alarm metadata...")
-        alarm_addr = await client.get_alarm_address(alarm_id)
-        alarm_state = await client.get_address_state(alarm_addr)
-        if alarm_state != "active":
+        if choice == 1:
+            client = await client_task
+            price = float(input("Enter the price: "))
+            await tick(client, price)
+            continue
+        if choice == 2:
+            client = await client_task
+            alarm_id = int(input("Enter the alarm id: "))
+            await ring(client, alarm_id)
+            continue
+        if choice == 3:
+            client = await client_task
+            options = f"latest: {client.metadata.total_alarms-1}"
+            alarm_id = int(input(f"Enter the alarm id ({options}): "))
+            print("Loading alarm metadata...")
+            alarm_addr = await client.get_alarm_address(alarm_id)
+            alarm_state = await client.get_address_state(alarm_addr)
+            if alarm_state != "active":
+                sys.stdout.write("\033[F")
+                print("Sorry, the alarm is not active, you can't wind it.")
+                continue
+            alarm_metadata = await client.get_alarm_metadata(alarm_addr)
+            remain_scale = alarm_metadata.remain_scale
+            decimal_ratio = 10 ** (client.metadata.base_asset_decimals - client.metadata.quote_asset_decimals)
+            old_price = alarm_metadata.base_asset_price * decimal_ratio / 2**64
             sys.stdout.write("\033[F")
-            print("Sorry, the alarm is not active, you can't wind it.")
-            return
-        alarm_metadata = await client.get_alarm_metadata(alarm_addr)
-        remain_scale = alarm_metadata.remain_scale
-        decimal_ratio = 10 ** (
-            client.metadata.base_asset_decimals - client.metadata.quote_asset_decimals
-        )
-        old_price = alarm_metadata.base_asset_price * decimal_ratio / 2**64
-        sys.stdout.write("\033[F")
-        if remain_scale == 0:
-            print("Sorry, the alarm remains 0 scale, you can't buy it.")
-            return
-        rng = "max: 1" if remain_scale == 1 else f"1~{remain_scale}"
-        buy_num = int(input(f"Enter the buy num ({rng}): "))
-        price = float(input(f"Enter the price (alarm price: {old_price}): "))
-        await wind(client, alarm_id, buy_num, price)
-    else:
-        print("Invalid choice")
+            if remain_scale == 0:
+                print("Sorry, the alarm remains 0 scale, you can't buy it.")
+                continue
+            rng = "max: 1" if remain_scale == 1 else f"1~{remain_scale}"
+            buy_num = int(input(f"Enter the buy num ({rng}): "))
+            if buy_num < 1 or buy_num > remain_scale:
+                print("Buy num out of range")
+                continue
+            price = float(input(f"Enter the price (alarm price: {old_price}): "))
+            await wind(client, alarm_id, buy_num, price)
+            continue
 
 
 if __name__ == "__main__":
